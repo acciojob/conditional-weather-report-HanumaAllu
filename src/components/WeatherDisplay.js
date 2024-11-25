cd0ae9023de15dec2e538cabb5166ad3e4677970
Allu Hanuma Reddy <82640761+HanumaAllu@users.noreply.github.com>
2024-11-25 10:43:01 +0530

    Update WeatherDisplay.js

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,1 +1,19 @@
+import React from "react";
+
+const WeatherDisplay = ({ weather }) => {
+  const { temperature, conditions } = weather;
+
+  
+  const temperatureColor = temperature > 20 ? 'red' : 'blue';
+
+  return (
+    <div>
+      {}
+      <p style={{ color: temperatureColor }}>Temperature: {temperature}</p>
+      <span style={{ color: 'black' }}>Conditions: {conditions}</span>
+    </div>
+  );
+};
+
+export default WeatherDisplay;
 
